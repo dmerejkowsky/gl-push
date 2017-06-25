a7b2f5cd124488bcce8e40619df18d6109124615
Dimitri Merejkowsky <d.merej@gmail.com>
2017-06-25 16:28:23 +0200

    failing test for gl_push.main()

--- gl_push/test/test_gl_push.py
+++ gl_push/test/test_gl_push.py
@@ -1,5 +1,28 @@
 import gl_push
+import gl_push.git
+import gl_push.gitlab
+
+from unittest import mock
 
 
-def test_zero():
-    gl_push.main()
+def test_push(git_server, monkeypatch, tmpdir):
+    url = git_server.create_repo("foo/bar")
+    monkeypatch.chdir(tmpdir)
+    foo_dir = tmpdir.ensure_dir("foo")
+    gl_push.git.run(foo_dir, "clone", url)
+    bar_src = foo_dir.join("bar")
+    bar_src.join("README").write("This is the README")
+    gl_push.git.run(bar_src, "add", ".")
+    gl_push.git.run(bar_src, "commit", "--message", "Initial commit")
+    gl_push.git.run(bar_src, "push", "origin", "master:master")
+    gl_push.git.run(bar_src, "checkout", "-b", "test1")
+    gl_push.git.run(bar_src, "commit", "--message", "test", "--allow-empty")
+
+    with mock.patch("gl_push.gitlab") as mock_gitlab:
+        monkeypatch.chdir(bar_src)
+        gl_push.main()
+        mock_gitlab.ensure_merge_request.assert_called_with(
+            "foo/bar",
+            "test1",
+            title="test1"
+        )
