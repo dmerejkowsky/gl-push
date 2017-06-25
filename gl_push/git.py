553d4da76d049aa7409e5d3e2cacb0d034ba623b
Dimitri Merejkowsky <d.merej@gmail.com>
2017-06-25 16:43:45 +0200

    Making the test pass

--- gl_push/git.py
+++ gl_push/git.py
@@ -1,5 +1,7 @@
 import subprocess
 
 
-def run(path, *cmd):
-    return subprocess.run(["git"] + list(cmd), cwd=path, check=True)
+def run(path, *cmd, capture=False):
+    stdout = subprocess.PIPE if capture else None
+    return subprocess.run(["git"] + list(cmd), cwd=path, check=True,
+                          stdout=stdout)
--- gl_push/main.py
+++ gl_push/main.py
@@ -1,2 +1,18 @@
+import os
+
+import gl_push.git
+import gl_push.gitlab
+
+
 def main():
-    pass
+    cwd = os.getcwd()
+    res = gl_push.git.run(cwd, "remote", "get-url", "origin",
+                          capture=True)
+    project = "/".join(res.stdout.decode().strip().split("/")[-2:])
+
+    res = gl_push.git.run(cwd, "rev-parse", "--abbrev-ref", "HEAD",
+                          capture=True)
+    branch = res.stdout.decode().strip()
+
+    title = branch
+    gl_push.gitlab.ensure_merge_request(project, branch, title=title)
